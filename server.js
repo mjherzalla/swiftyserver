ca22f7fbe7c143d8550bd4026e1b375066b663cb
mjherzalla <mjherzalla@outlook.com>
2021-10-15 14:54:32 -0700

    first push

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
-const jsonFile = require("./model");
+
+const boardModel = require("./Models/boardModel");
+const cardModel = require("./Models/cardModel");
+const listModel = require("./Models/listModel");
+const userModel = require("./Models/userModal");
+
 const router = express.Router();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,7 +18,7 @@ app.use(function (req, res, next) {
   next();
 });
 const uri =
-  "mongodb+srv://milagro:moon2006@cluster0.vcm9w.mongodb.net/jsonhost?retryWrites=true&w=majority";
+  "mongodb+srv://milagro:moon2006@cluster0.vcm9w.mongodb.net/osmos?retryWrites=true&w=majority";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -28,39 +33,85 @@ connection.once("open", function () {
 
 app.use("/", router);
 
-router.route("/CreateNew").get(function (req, res) {
-  console.log(req.query.data);
-  const Jdata = JSON.parse(req.query.data);
-  console.log(Jdata);
+router.route("/createNewboard").get(function (req, res) {
   var data = {
-    userID: Jdata[0].userID || "UserID",
-    fileID: Jdata[0].fileID || "filedi",
-    title: Jdata[0].title || "filedi",
-    apiUrl: Jdata[0].apiUrl || "filedi",
-    data: JSON.stringify(Jdata[0].data) || "filedi",
+    boardID: "1",
+    boardTitle: "boardTitle",
+    users: [{ userName: "newuser", avatarColor: "piclink" }],
+    pojectIconUrl: "pojectIconUrl",
   };
 
-  const newj = new jsonFile(data);
+  const newboard = new boardModel(data);
 
-  newj.save(function (err) {
+  newboard.save(function (err) {
     if (err) return console.log(err);
     // saved!
+    res.send("New board created succesfully");
   });
 });
 
-router.route("/File").get(function (req, res) {
-  // find all athletes that play tennis
-  var query = jsonFile.find({ fileID: req.query.fileID });
+router.route("/createNewList").get(function (req, res) {
+  var data = {
+    listID: req.query.listID || "",
+    listTitle: req.query.listTitle || "title",
+    listStatus: req.query.listStatus || "On",
+  };
+  const newList = new listModel(data);
 
-  query.exec(function (err, file) {
-    if (file.length == 0) {
-      res.send("file notfound");
-    } else {
-      res.send(file[0].data);
-    }
+  newList.save(function (err) {
+    if (err) return console.log(err);
+    // saved!
+    res.send("New list created Succesfully");
+  });
+});
 
+router.route("/createNewCard").get(function (req, res) {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+
+  var data = {
+    cardID: Math.random().toString(36).substr(2, 9),
+    listID: req.query.listID,
+    title: req.query.title,
+    description: "description",
+    status: "status",
+    assignedTo: { userName: "none", avatarColor: "" },
+    date: month + "/" + day,
+    priority: req.query.priority,
+  };
+  const newCard = new cardModel(data);
+
+  newCard.save(function (err) {
+    if (err) return console.log(err);
+    // saved!
+    res.send("New card created Succesfully");
+  });
+});
+router.route("/getLists").get(function (req, res) {
+  listModel.find({}, function (err, lists) {
     if (err) return handleError(err);
-    // athletes contains an ordered list of 5 athletes who play Tennis
+    res.send(lists);
+  });
+});
+
+router.route("/getCards").get(function (req, res) {
+  cardModel.find({}, function (err, lists) {
+    if (err) return handleError(err);
+    res.send(lists);
+  });
+});
+
+router.route("/getBoards").get(function (req, res) {
+  boardModel.find({}, function (err, lists) {
+    if (err) return handleError(err);
+    res.send(lists);
+  });
+});
+router.route("/getUsers").get(function (req, res) {
+  userModel.find({}, function (err, lists) {
+    if (err) return handleError(err);
+    res.send(lists);
   });
 });
 
@@ -75,24 +126,8 @@ router.route("/deleteFile").get(function (req, res) {
   });
 });
 
-router.route("/getFilesList").get(function (req, res) {
-  // find all athletes that play tennis
-  var query = jsonFile.find({ userID: req.query.userID });
-
-  query.exec(function (err, file) {
-    if (file.length == 0) {
-      res.send("file notfound");
-    } else {
-      res.send(file);
-    }
-
-    if (err) return handleError(err);
-    // athletes contains an ordered list of 5 athletes who play Tennis
-  });
-});
-router.route("/UpdateFile").get(function (req, res) {
+router.route("/updateboard").get(function (req, res) {
   var query = { fileID: req.query.fileID };
-  console.log("sss");
 
   jsonFile.findOneAndUpdate(
     query,
@@ -103,6 +138,68 @@ router.route("/UpdateFile").get(function (req, res) {
       return res.send("Succesfully saved.");
     }
   );
+});
+router.route("/updateCardListID").get(function (req, res) {
+  var query = { cardID: req.query.cardID };
+
+  cardModel.findOneAndUpdate(
+    query,
+    { listID: req.query.listID },
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return res.send("Succesfully saved.");
+    }
+  );
+});
+router.route("/assginUsertoCard").get(function (req, res) {
+  var query = { cardID: req.query.cardID };
+  console.log(req.query.avatarColor);
+  cardModel.findOneAndUpdate(
+    query,
+    {
+      assignedTo: {
+        userName: req.query.userName,
+        avatarColor: "#" + req.query.avatarColor,
+      },
+    },
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return res.send("Succesfully saved.");
+    }
+  );
+});
+
+router.route("/updateListTitle").get(function (req, res) {
+  var query = { listID: req.query.listID };
+
+  listModel.findOneAndUpdate(
+    query,
+    { listTitle: req.query.listTitle },
+    { upsert: true },
+    function (err, doc) {
+      if (err) return res.send(500, { error: err });
+      return res.send("Succesfully saved.");
+    }
+  );
+});
+
+router.route("/addUserToBorad").get(function (req, res) {
+  var query = { boradID: req.query.boardID };
+
+  var data = {
+    userName: req.query.userName,
+    avatarColor: "#" + req.query.avatarColor,
+  };
+
+  const newUser = new userModel(data);
+
+  newUser.save(function (err) {
+    if (err) return console.log(err);
+    // saved!
+    res.send("New user created succesfully");
+  });
 });
 
 app.listen(process.env.PORT || 3001, function () {});
